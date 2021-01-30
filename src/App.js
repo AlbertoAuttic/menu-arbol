9ee1d4306aa04262f6a0b3033019d93178009296
Ian-syah <okta.yoga.syah@gmail.com>
2021-01-30 11:38:15 +0800

    :exclamation: Add Dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
+
 import { ReactComponent as HomeIcon} from './icons/home.svg';
+import { ReactComponent as AboutIcon} from './icons/about.svg';
+import { ReactComponent as CaretIcon} from './icons/caret.svg';
+import { ReactComponent as TutorialIcon} from './icons/tutorial.svg';
 
 const App = () =>{
   return (
     <NavBar>
       <NavItem icon={<HomeIcon />}/>
+      <NavItem icon={<AboutIcon />}/>
+      {/* <NavItem icon={<TutorialIcon />}/> */}
+      
+      <NavItem icon={<CaretIcon />} > 
+        <DropDownMenu/>
+        
+      </NavItem>
+
     </NavBar>
   );
 };
@@ -19,13 +31,43 @@ const NavBar = (props) => {
 };
 
 const NavItem = (props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
   );
+};
+
+const DropDownMenu = () => {
+  
+  const DropDownItem = (props) => {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+
+        {props.children}
+        
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  };
+  
+  return (
+    <div className="dropdown">
+      <DropDownItem>Test</DropDownItem>
+      <DropDownItem 
+        leftIcon={<TutorialIcon />}>
+        Tutorial
+      </DropDownItem>
+
+    </div>
+  )
 };
 
 export default App;
