4c19abc4256d0bb833e9cc6f6bf93896a264b6d8
Ian-syah <okta.yoga.syah@gmail.com>
2021-01-31 16:12:25 +0800

    :cloud: Add two menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+import './index.css';
 import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import { ReactComponent as HomeIcon} from './icons/home.svg';
 import { ReactComponent as AboutIcon} from './icons/about.svg';
 import { ReactComponent as CaretIcon} from './icons/caret.svg';
 import { ReactComponent as TutorialIcon} from './icons/tutorial.svg';
+import { ReactComponent as SearchIcon} from './icons/search.svg';
+import { ReactComponent as SettingIcon} from './icons/setting.svg';
+import { ReactComponent as LogoutIcon} from './icons/logout.svg';
+import { ReactComponent as ProfileIcon} from './icons/profile.svg';
+import { ReactComponent as LeftArrow} from './icons/leftArrow.svg';
+import { ReactComponent as RightArrow} from './icons/morethan.svg';
+import { ReactComponent as PythonIcon} from './icons/python.svg';
+import { ReactComponent as HtmlIcon} from './icons/html.svg';
 
 const App = () =>{
   return (
     <NavBar>
-      <NavItem icon={<HomeIcon />}/>
-      <NavItem icon={<AboutIcon />}/>
-      {/* <NavItem icon={<TutorialIcon />}/> */}
-      
+      <NavItem icon={<HomeIcon />} title="Home"/>
+      <NavItem icon={<SearchIcon />} title="Search"/>
       <NavItem icon={<CaretIcon />} > 
-        <DropDownMenu/>
+        <DropDownMenu></DropDownMenu>
         
       </NavItem>
 
     </NavBar>
   );
 };
-
 
 const NavBar = (props) => {
   return (
@@ -35,23 +42,32 @@ const NavItem = (props) => {
 
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+      <a href="#" className="icon-button" title={props.title} onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
 
       {open && props.children}
+
     </li>
   );
 };
 
 const DropDownMenu = () => {
+
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calc_height(el){
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
   
   const DropDownItem = (props) => {
     return (
-      <a href="#" className="menu-item">
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         <span className="icon-button">{props.leftIcon}</span>
 
-        {props.children}
+        <p>{props.children}</p>
         
         <span className="icon-right">{props.rightIcon}</span>
       </a>
@@ -59,13 +75,53 @@ const DropDownMenu = () => {
   };
   
   return (
-    <div className="dropdown">
-      <DropDownItem>Test</DropDownItem>
-      <DropDownItem 
-        leftIcon={<TutorialIcon />}>
-        Tutorial
-      </DropDownItem>
+    <div className="dropdown" style={{ height: menuHeight}}>
+      <CSSTransition 
+      in={activeMenu === 'main'} 
+      unmountOnExit timeout={500} 
+      classNames="menu-primary" 
+      onEnter={calc_height}
+      >
+          <div className="menu">
+            <DropDownItem 
+            leftIcon={<SettingIcon/>}
+            rightIcon={<RightArrow />}
+            goToMenu='setting'>
+              Setting
+            </DropDownItem>
+            <DropDownItem 
+              leftIcon={<TutorialIcon />}
+              rightIcon={<RightArrow />}
+              goToMenu='tutorial'>
+              Tutorial
+            </DropDownItem>
+            <DropDownItem leftIcon={<AboutIcon/>}>About</DropDownItem>
+            <DropDownItem leftIcon={<LogoutIcon/>}>
+              Keluar
+            </DropDownItem>
+          </div>
+      </CSSTransition>
 
+      <CSSTransition in={activeMenu === 'tutorial'} unmountOnExit timeout={500} classNames="menu-secondary" onEnter={calc_height}>
+          <div className="menu">
+            <DropDownItem leftIcon={<LeftArrow />} goToMenu='main'/>
+            <DropDownItem leftIcon={<PythonIcon/>}>
+              Python
+            </DropDownItem>
+            <DropDownItem leftIcon={<HtmlIcon/>}>
+              HTML
+            </DropDownItem>
+          </div>
+      </CSSTransition>
+      
+      <CSSTransition in={activeMenu === 'setting'} unmountOnExit timeout={500} classNames="menu-secondary" onEnter={calc_height}>
+          <div className="menu">
+            <DropDownItem leftIcon={<LeftArrow />} goToMenu='main'/>
+            <DropDownItem leftIcon={<ProfileIcon/>}>
+              Ubah Profile
+            </DropDownItem>
+          </div>
+      </CSSTransition>
     </div>
   )
 };
