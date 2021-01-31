82ae1ae68bca04a4fe0ba08988c7e1cd4ce32158
Ian-syah <okta.yoga.syah@gmail.com>
2021-01-31 21:47:10 +0800

    :school: Add More Menu

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import React, { useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 import { ReactComponent as HomeIcon} from './icons/home.svg';
-import { ReactComponent as AboutIcon} from './icons/about.svg';
-import { ReactComponent as CaretIcon} from './icons/caret.svg';
-import { ReactComponent as TutorialIcon} from './icons/tutorial.svg';
 import { ReactComponent as SearchIcon} from './icons/search.svg';
+import { ReactComponent as CaretIcon} from './icons/caret.svg';
+
 import { ReactComponent as SettingIcon} from './icons/setting.svg';
+import { ReactComponent as TutorialIcon} from './icons/tutorial.svg';
+import { ReactComponent as AboutIcon} from './icons/about.svg';
 import { ReactComponent as LogoutIcon} from './icons/logout.svg';
-import { ReactComponent as ProfileIcon} from './icons/profile.svg';
+
 import { ReactComponent as LeftArrow} from './icons/leftArrow.svg';
 import { ReactComponent as RightArrow} from './icons/morethan.svg';
+
+import { ReactComponent as ProfileIcon} from './icons/profile.svg';
+import { ReactComponent as PasswordIcon} from './icons/password.svg';
+
 import { ReactComponent as PythonIcon} from './icons/python.svg';
+import { ReactComponent as CppIcon} from './icons/cpp.svg';
+import { ReactComponent as JavaIcon} from './icons/java.svg';
+import { ReactComponent as SqlIcon} from './icons/sql.svg';
 import { ReactComponent as HtmlIcon} from './icons/html.svg';
+import { ReactComponent as CssIcon} from './icons/css.svg';
+import { ReactComponent as JsIcon} from './icons/javascript.svg';
 
 const App = () =>{
   return (
@@ -42,7 +52,7 @@ const NavItem = (props) => {
 
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button" title={props.title} onClick={() => setOpen(!open)}>
+      <a href="#" className="icon-button" title={props.title} onClick={() => setOpen(!open)} >
         {props.icon}
       </a>
 
@@ -64,7 +74,7 @@ const DropDownMenu = () => {
   
   const DropDownItem = (props) => {
     return (
-      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)} >
         <span className="icon-button">{props.leftIcon}</span>
 
         <p>{props.children}</p>
@@ -82,6 +92,7 @@ const DropDownMenu = () => {
       classNames="menu-primary" 
       onEnter={calc_height}
       >
+          {/* Menu Primary */}
           <div className="menu">
             <DropDownItem 
             leftIcon={<SettingIcon/>}
@@ -102,23 +113,43 @@ const DropDownMenu = () => {
           </div>
       </CSSTransition>
 
+      {/* Menu Tutorial */}
       <CSSTransition in={activeMenu === 'tutorial'} unmountOnExit timeout={500} classNames="menu-secondary" onEnter={calc_height}>
           <div className="menu">
-            <DropDownItem leftIcon={<LeftArrow />} goToMenu='main'/>
+            <DropDownItem leftIcon={<LeftArrow />} goToMenu='main'>Tutorial</DropDownItem>
             <DropDownItem leftIcon={<PythonIcon/>}>
               Python
+            </DropDownItem>
+            <DropDownItem leftIcon={<CppIcon/>}>
+              C++
+            </DropDownItem>
+            <DropDownItem leftIcon={<JavaIcon/>}>
+              Java
+            </DropDownItem>
+            <DropDownItem leftIcon={<SqlIcon/>}>
+              MySQL
             </DropDownItem>
             <DropDownItem leftIcon={<HtmlIcon/>}>
               HTML
             </DropDownItem>
+            <DropDownItem leftIcon={<CssIcon/>}>
+              CSS
+            </DropDownItem>
+            <DropDownItem leftIcon={<JsIcon/>}>
+              Javascript
+            </DropDownItem>
           </div>
       </CSSTransition>
-      
+
+      {/* Menu Setting */}
       <CSSTransition in={activeMenu === 'setting'} unmountOnExit timeout={500} classNames="menu-secondary" onEnter={calc_height}>
           <div className="menu">
-            <DropDownItem leftIcon={<LeftArrow />} goToMenu='main'/>
+            <DropDownItem leftIcon={<LeftArrow />} goToMenu='main'>Setting</DropDownItem>
             <DropDownItem leftIcon={<ProfileIcon/>}>
               Ubah Profile
+            </DropDownItem>
+            <DropDownItem leftIcon={<PasswordIcon/>}>
+              Ubah Password
             </DropDownItem>
           </div>
       </CSSTransition>
